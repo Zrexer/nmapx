57e69eb999572761f5a55d10d088c02bd4f762f6
Zrexer <122536817+Zrexer@users.noreply.github.com>
2024-02-26 16:59:27 +0330

    Add files via upload

---
+++ Nmapx/binary/nmapx.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'nmapx.exe' with Python DLL being found.
+set PATH=C:\Users\ELMGOS~1\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311
+set NUITKA_PYTHONPATH=G:\__backtrack__\Nmapx;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\Lib;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\DLLs;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\Lib\site-packages;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32\lib;C:\Users\ELMGOSTAR\AppData\Local\Programs\Python\Python311\Lib\site-packages\Pythonwin
+"%~dp0nmapx.exe" %*
